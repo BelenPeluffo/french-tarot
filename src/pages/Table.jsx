713e495f2416fc61366e1e1a5db3e1288a86db68
Belén Peluffo <belen.peluffo@dirmod.com>
2024-04-18 17:56:22 -0300

    feat: creates logic for selecting cards and style to show cards names in table.

--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -1,5 +1,73 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const CARDS_DECK = ["♠", "♥", "♦", "♣"];
+const CARDS_VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
 const Table = () => {
-  return <div>Table</div>;
+  const [selectedCards, setSelectedCards] = useState(null);
+
+  const getRandomCard = () => {
+    const deck =
+      CARDS_DECK[Math.round(Math.random() * (CARDS_DECK.length - 1))];
+    const value =
+      CARDS_VALUES[Math.round(Math.random() * (CARDS_VALUES.length - 1))];
+    return `${value} ${deck}`;
+  };
+
+  const setCards = () => {
+    const card = getRandomCard();
+    if (!selectedCards.includes(card)) {
+      setSelectedCards([...selectedCards, card]);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedCards && selectedCards.length < 4) {
+      setCards();
+    }
+  }, [selectedCards]);
+
+  return (
+    <div>
+      <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+        <div className="card-container">
+          <div>Past</div>
+          <div className="card">{selectedCards ? selectedCards[0] : null}</div>
+        </div>
+        <div className="card-container">
+          <div>Present</div>
+          <div className="card">{selectedCards ? selectedCards[1] : null}</div>
+        </div>
+        <div className="card-container">
+          <div>Future</div>
+          <div className="card">{selectedCards ? selectedCards[2] : null}</div>
+        </div>
+      </div>
+      <div>
+        <button
+          style={{ marginTop: "10px" }}
+          onClick={() => setSelectedCards([])}
+        >
+          Show cards
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Table;
